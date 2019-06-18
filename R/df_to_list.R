12e6f1997ed2f3c885e12eae34d71c149d7c2d07
HP01\Gregor <gregor.pirs@fri.uni-lj.si>
2019-06-18 16:20:13 +0200

    - Added core attribute names checking. - Added covariates names checking.

--- R/df_to_list.R
+++ R/df_to_list.R
@@ -8,14 +8,100 @@
 #' @return A list suitable for transformation to csv.
 #'
 #' @examples
-df_to_list <- function (df, covariates) {
+df_to_list <- function (df,
+                        CovariatesX    = NULL,
+                        CovariatesY    = NULL,
+                        CovariatesZ    = NULL,
+                        SubjectIdVar   = NULL,
+                        StudyIdVar     = NULL,
+                        TimeVar        = NULL,
+                        ScoreVar       = NULL,
+                        SecondScoreVar = NULL,
+                        covariates) {
+  # assign names
+  IDp   <- deparse(substitute(SubjectIdVar))
+  IDs   <- deparse(substitute(StudyIdVar))
+  times <- deparse(substitute(TimeVar))
+  S     <- deparse(substitute(ScoreVar))
+  if (IDp == "NULL") {
+    IDp <- "SubjectId"
+  } 
+  if (IDs == "NULL") {
+    IDs <- "StudyId"
+  } 
+  if (times == "NULL") {
+    times <- "Time"
+  } 
+  if (S == "NULL") {
+    S <- "Score"
+  } 
+  
+  # check if in data frame
+  cnames <- colnames(df)
+  if (!(IDp %in% cnames)) {
+    stop(paste0("No column with name ", IDp, " in the data frame."))
+  }
+  if (!(IDs %in% cnames)) {
+    stop(paste0("No column with name ", IDs, " in the data frame."))
+  }
+  if (!(times %in% cnames)) {
+    stop(paste0("No column with name ", times, " in the data frame."))
+  }
+  if (!(S %in% cnames)) {
+    stop(paste0("No column with name ", S, " in the data frame."))
+  }
+  
+  # check second score
+  S2 <- deparse(substitute(SecondScoreVar))
+  if (S2 != "NULL") {
+    if (S2 == S) {
+      stop("Second score has the same name as the first score.")
+    }
+    if (!(S2 %in% cnames)) {
+      stop(paste0("No column with name ", S2, " in the data frame."))
+    }
+  }
+  
+  # covariates
+  covx_nms <- all.vars(CovariatesX)
+  covy_nms <- all.vars(CovariatesY)
+  covz_nms <- all.vars(CovariatesZ)
+  if (length(intersect(covx_nms, covy_nms)) > 0) {
+    stop("Non-empty intersection between covariates X and Y.")
+  }
+  if (length(intersect(covx_nms, covz_nms)) > 0) {
+    stop("Non-empty intersection between covariates X and Z.")
+  }
+  if (length(intersect(covy_nms, covz_nms)) > 0) {
+    stop("Non-empty intersection between covariates Y and Z.")
+  }
+  other_names <- c(IDp, IDs, times, S)
+  if (S2 != "NULL") {
+    other_names <- c(other_names, S2)
+  }
+  if (length(intersect(covx_nms, other_names)) > 0) {
+    stop("Non-empty intersection between covariates X and core attributes.")
+  }
+  if (length(intersect(covy_nms, other_names)) > 0) {
+    stop("Non-empty intersection between covariates Y and core attributes.")
+  }
+  if (length(intersect(covz_nms, other_names)) > 0) {
+    stop("Non-empty intersection between covariates Z and core attributes.")
+  }
+  
+  
+  # transform to long format!!!
+  # TODO
+  
+  # other
   cov_nms <- all.vars(covariates)
   print(cov_nms)
   N       <- nrow(df)
-  P       <- length(unique(df$IDp))
-  M       <- length(unique(df$IDs))
-  IDp     <- df$IDp
-  IDs     <- df$IDs
+  P       <- length(unique(df[ ,IDp]))
+  M       <- length(unique(df[ ,IDs]))
+  IDp     <- df[ ,IDp]
+  IDs     <- df[ ,IDs]
+  
   tmp_list <- nlist()
   for (i in 1:length(cov_nms)) {
     # tmp <- covariates[i]
@@ -24,11 +110,16 @@ df_to_list <- function (df, covariates) {
     tmp_list[[i]] <- df[ ,tmp]
   }
   names(tmp_list) <- cov_nms
-  time <- df$time
-  S    <- df$S
+  times <- df[ ,times]
+  S     <- df[ ,S]
+  
+  
+  
+  
+  
   out_list <- c(nlist(N, P, M, IDp, IDs), 
                 tmp_list,
-                nlist(time, S))
+                nlist(times, S))
   return(out_list)
   
 }
--- tests/utilities/test_list_to_df.R
+++ tests/utilities/test_list_to_df.R
@@ -6,8 +6,9 @@ setwd(my_path)
 library(loo)
 source("./data/generalized_logistic_model/data.R")
 source("./R/list_to_df.R")
+source("./R/df_to_list.R")
 
-
+# to df ------------------------------------------------------------------------
 in_data <- nlist(N,
                  P,
                  M,
@@ -24,7 +25,68 @@ df <- list_to_df(in_data)
 head(df)
 
 
-# my_list <- df_to_list(df, c("AGE", "SEX", "COMED", "APOE4"))
-my_list <- df_to_list(df, ~ AGE + SEX + COMED + APOE4)
+# test parameter names ---------------------------------------------------------
+
+# ok
+my_list <- df_to_list(df,
+                      SubjectIdVar   = IDp,
+                      StudyIdVar     = IDs,
+                      TimeVar        = time,
+                      ScoreVar       = S,
+                      covariates = ~ AGE + SEX + COMED + APOE4)
 names(my_list)
 my_list
+
+# one name not in df
+my_list <- df_to_list(df,
+                      SubjectIdVar   = IDa,
+                      StudyIdVar     = IDs,
+                      TimeVar        = time,
+                      ScoreVar       = S,
+                      covariates = ~ AGE + SEX + COMED + APOE4)
+
+# missing one name
+my_list <- df_to_list(df,
+                      SubjectIdVar   = IDp,
+                      TimeVar        = time,
+                      ScoreVar       = S,
+                      covariates = ~ AGE + SEX + COMED + APOE4)
+
+# second score same name
+my_list <- df_to_list(df,
+                      SubjectIdVar   = IDp,
+                      StudyIdVar     = IDs,
+                      TimeVar        = time,
+                      ScoreVar       = S,
+                      SecondScoreVar = S,
+                      covariates = ~ AGE + SEX + COMED + APOE4)
+
+# second score not in df
+my_list <- df_to_list(df,
+                      SubjectIdVar   = IDp,
+                      StudyIdVar     = IDs,
+                      TimeVar        = time,
+                      ScoreVar       = S,
+                      SecondScoreVar = S2,
+                      covariates = ~ AGE + SEX + COMED + APOE4)
+
+
+# covariates -------------------------------------------------------------------
+# same covariates for X and Y
+my_list <- df_to_list(df,
+                      SubjectIdVar   = IDp,
+                      StudyIdVar     = IDs,
+                      TimeVar        = time,
+                      ScoreVar       = S,
+                      CovariatesX    = ~ AGE + SEX,
+                      CovariatesY    = ~ SEX + COMED,
+                      covariates = ~ AGE + SEX + COMED + APOE4)
+
+# covariate in core attributes
+my_list <- df_to_list(df,
+                      SubjectIdVar   = IDp,
+                      StudyIdVar     = IDs,
+                      TimeVar        = time,
+                      ScoreVar       = S,
+                      CovariatesX    = ~ AGE + SEX + IDp,
+                      covariates = ~ AGE + SEX + COMED + APOE4)
