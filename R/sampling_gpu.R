55906e5bfa4249b67d4ac440ef82f99c5d57c0c8
gregorp90 <gregor.pirs1@gmail.com>
2019-12-18 08:31:58 +0100

    - Updated R interface.

--- R/sampling_gpu.R
+++ R/sampling_gpu.R
@@ -4,8 +4,8 @@
 #' only used in case of 2 scores.
 #'
 #' @param df Input data frame.
-#' @param mod_name character. The name of the model as saved in 
-#' "./bin/generalized_logistic_model/Win64/" or 
+#' @param mod_name character. The name of the model as saved in
+#' "./bin/generalized_logistic_model/Win64/" or
 #' "./bin/generalized_logistic_model/Linux/", without the extension.
 #' @param SubjectIdVar Name of the column which holds the subject IDs.
 #' Must be without quotation marks.
@@ -24,7 +24,7 @@
 #' are multiplicative.
 #' @param m_b binary. 0 if the covariates for baseline are additive, 1 if they
 #' are multiplicative.
-#' @param ScoreVar2 Name of the column which holds the second scores 
+#' @param ScoreVar2 Name of the column which holds the second scores
 #' (responses) if applicable.
 #' Must be without quotation marks.
 #' @param is_pbo2 binary vector. 1 for placebo studies, 0 for others.
@@ -44,12 +44,13 @@
 #' @param seed integer. Random seed for the model.
 #' @param ... Other parameters for the sampler.
 #'
-#' @return A list with 3 elements. First is an object of stanfit. Second is the
+#' @return A list with 4 elements. First is an object of stanfit. Second is the
 #' data used in the modeling. Third are the maps from original subject and
-#' study IDs to the IDs used in the model.
+#' study IDs to the IDs used in the model. Fourth are the predicted values for
+#' each iteration.
 #'
 sampling_gpu        <- function (df,
-                                 mod_name        = "generalized_logistic_model",
+                                 mod_name        = "glm_one_score_packed",
                                  SubjectIdVar    = IDp,
                                  StudyIdVar      = IDs,
                                  TimeVar         = time,
@@ -83,13 +84,13 @@ sampling_gpu        <- function (df,
                                  engine          = "nuts",
                                  max_depth       = 10,
                                  metric          = "diag_e",
-                                 metric_file     =  "",
+                                 metric_file     = "",
                                  stepsize        = 1 ,
                                  stepsize_jitter = 0,
                                  id              = 0,
                                  seed            = 1607674300,
                                  ...) {
-  df_to_list <- function (df, 
+  df_to_list <- function (df,
                           IDp            = NULL,
                           IDs            = NULL,
                           times          = NULL,
@@ -104,12 +105,12 @@ sampling_gpu        <- function (df,
                           covb2_nms      = NULL,
                           m_r2           = NULL,
                           m_b2           = NULL,
-                          is_pbo2        = NULL,
-                          mod_name
+                          is_pbo2        = NULL
   ) {
+    # browser()
     s1_ind <- df[ ,score]
     df1    <- df[!is.na(s1_ind), ]
-    
+
     # Check for NAs and NULL
     df_vars <- df1[ ,c(IDp, IDs, times, score, covr_nms, covb_nms, is_pbo)]
     if(any(is.na(df_vars))) {
@@ -118,11 +119,11 @@ sampling_gpu        <- function (df,
     if(any(is.null(df_vars))) {
       stop("NULL values in the data frame for score 1.")
     }
-    
+
     N1      <- nrow(df1)
     P1      <- length(unique(df1[ ,IDp]))
     M1      <- length(unique(df1[ ,IDs]))
-    
+
     # re-index
     reindex <- function  (x) {
       new_inds <- x
@@ -137,73 +138,54 @@ sampling_gpu        <- function (df,
     IDp1      <- reindex(IDp1)
     reindexed <- IDp1
     IDp1_map  <- unique(data.frame(original, reindexed))
-    
+
     # studies
     IDs1      <- df1[ ,IDs]
     original  <- IDs1
     IDs1      <- reindex(IDs1)
     reindexed <- IDs1
     IDs1_map  <- unique(data.frame(original, reindexed))
-    
+
     map_list <- list(
       "patient1" = IDp1_map,
       "study1"   = IDs1_map
     )
-    
+
     # other variables
     times1  <- df1[ ,times]
     S1      <- df1[ ,score]
     covsr1  <- as.matrix(df1[ ,covr_nms])
     covsb1  <- as.matrix(df1[ ,covb_nms])
     is_pbo1 <- df1[ ,is_pbo]
-    
+
     # get placebo studies
     tmp     <- data.frame(IDs1, is_pbo1)
     tmp     <- unique(tmp)
     is_pbo1 <- tmp[order(tmp[ ,1]),2]
-    
-    # CORRECT THIS!!!
-    if (mod_name == "glm_one_score_packed") {
-      out_list <- list(
-        N                = N1,
-        P                = P1,
-        M                = M1,
-        Q_r              = ncol(covsr1),
-        Q_s              = ncol(covsb1),
-        multiplicative_s = m_b,
-        multiplicative_r = m_r,
-        X_r              = covsr1,
-        X_s              = covsb1,
-        is_pbo           = is_pbo1,
-        IDp              = IDp1,
-        IDs              = IDs1,
-        time             = times1,
-        score1           = S1
-      )
-    } else {
-      out_list <- list(
-        N                = N1,
-        P                = P1,
-        M                = M1,
-        Q_r              = ncol(covsr1),
-        Q_s              = ncol(covsb1),
-        multiplicative_s = m_b,
-        multiplicative_r = m_r,
-        X_r              = covsr1,
-        X_s              = covsb1,
-        is_pbo           = is_pbo1,
-        IDp              = IDp1,
-        IDs              = IDs1,
-        time             = times1,
-        score            = S1
-      )
-    }
-    
-    
+
+
+    out_list <- list(
+      N                = N1,
+      P                = P1,
+      M                = M1,
+      Q_r              = ncol(covsr1),
+      Q_s              = ncol(covsb1),
+      multiplicative_s = m_b,
+      multiplicative_r = m_r,
+      X_r              = covsr1,
+      X_s              = covsb1,
+      is_pbo           = is_pbo1,
+      IDp              = IDp1,
+      IDs              = IDs1,
+      time             = times1,
+      score1           = S1
+    )
+
+
     if (!is.null(score2)) {
       s2_ind <- df[ ,score2]
       df2    <- df[!is.na(s2_ind), ]
-      
+
       df_vars <- df2[ ,c(IDp, IDs, times, score2, covr2_nms, covb2_nms, is_pbo2)]
       if(any(is.na(df_vars))) {
         stop("Missing values in the data frame for score 2.")
@@ -211,11 +193,11 @@ sampling_gpu        <- function (df,
       if(any(is.null(df_vars))) {
         stop("NULL values in the data frame for score 2.")
       }
-      
+
       N2      <- nrow(df2)
       P2      <- length(unique(df2[ ,IDp]))
       M2      <- length(unique(df2[ ,IDs]))
-      
+
       # reindex
       # patients
       IDp2      <- df2[ ,IDp]
@@ -223,33 +205,55 @@ sampling_gpu        <- function (df,
       IDp2      <- reindex(IDp2)
       reindexed <- IDp2
       IDp2_map  <- unique(data.frame(original, reindexed))
-      
+
       # studies
       IDs2      <- df2[ ,IDs]
       original  <- IDs2
       IDs2      <- reindex(IDs2)
       reindexed <- IDs2
       IDs2_map  <- unique(data.frame(original, reindexed))
-      
+
       map_list <- list(
         "patient1" = IDp1_map,
         "study1"   = IDs1_map,
         "patient2" = IDp2_map,
         "study2"   = IDs2_map
       )
-      
+
       times2  <- df2[ ,times]
       S2      <- df2[ ,score2]
       covsr2  <- as.matrix(df2[ ,covr2_nms])
       covsb2  <- as.matrix(df2[ ,covb2_nms])
       is_pbo2 <- df2[ ,is_pbo2]
-      
+
       # get placebo studies
       tmp     <- data.frame(IDs2, is_pbo2)
       tmp     <- unique(tmp)
       is_pbo2 <- tmp[order(tmp[ ,1]),2]
-      
-      
+
+
+      # Prepare mapping of patients for stan
+      have_both <- base::intersect(map_list$patient1$original,
+                                   map_list$patient2$original)
+      pmap_stan <- data.frame("original" = have_both)
+      pmap_stan <- merge(pmap_stan, map_list$patient1, by = "original")
+      pmap_stan <- merge(pmap_stan, map_list$patient2, by = "original")
+      pmap_stan <- pmap_stan[ ,c("reindexed.x", "reindexed.y")]
+
+      pother1 <- map_list$patient1$reindexed[!(map_list$patient1$original %in% have_both)]
+      pother2 <- map_list$patient2$reindexed[!(map_list$patient2$original %in% have_both)]
+
+      # prepare mapping of studies for stan
+      have_both <- base::intersect(map_list$study1$original,
+                                   map_list$study2$original)
+      smap_stan <- data.frame("original" = have_both)
+      smap_stan <- merge(smap_stan, map_list$study1, by = "original")
+      smap_stan <- merge(smap_stan, map_list$study2, by = "original")
+      smap_stan <- smap_stan[ ,c("reindexed.x", "reindexed.y")]
+
+      sother1 <- map_list$study1$reindexed[!(map_list$study1$original %in% have_both)]
+      sother2 <- map_list$study2$reindexed[!(map_list$study2$original %in% have_both)]
+
       out_list <- list(
         N                 = N1,
         P                 = P1,
@@ -264,7 +268,8 @@ sampling_gpu        <- function (df,
         IDp               = IDp1,
         IDs               = IDs1,
         time              = times1,
-        score             = S1,
+        score1            = S1,
+
         N2                = N2,
         P2                = P2,
         M2                = M2,
@@ -278,7 +283,16 @@ sampling_gpu        <- function (df,
         IDp2              = IDp2,
         IDs2              = IDs2,
         time2             = times2,
-        score2            = S2
+        score2            = S2,
+
+        Pn              = nrow(pmap_stan),
+        Sn              = nrow(smap_stan),
+        patient_map     = pmap_stan,
+        study_map       = smap_stan,
+        other_patients1 = pother1,
+        other_patients2 = pother2,
+        other_studies1  = as.array(sother1),
+        other_studies2  = as.array(sother2)
       )
     }
     tmp_out <- list(
@@ -294,7 +308,7 @@ sampling_gpu        <- function (df,
   times      <- deparse(substitute(TimeVar))
   score      <- deparse(substitute(ScoreVar))
   is_pbo     <- deparse(substitute(is_pbo))
-  
+
 
   # check if in data frame
   cnames <- colnames(df)
@@ -317,7 +331,7 @@ sampling_gpu        <- function (df,
   # covariates
   covr_nms    <- all.vars(CovariatesR)
   covb_nms    <- all.vars(CovariatesB)
-  
+
   if (any(!(covr_nms %in% cnames))) {
     stop("No column with CovariatesR in the data frame.")
   }
@@ -358,28 +372,27 @@ sampling_gpu        <- function (df,
   if (length(intersect(covb_nms, other_names)) > 0) {
     stop("Non-empty intersection between baseline covariates and core attributes.")
   }
-  
-  
+
+
   # create temporary file names for data and init
   data_file <- paste0(tempdir(),
                       "/data",
-                      paste(c(sample(c("a","b","c","d", 1:9), 4, TRUE)), 
+                      paste(c(sample(c("a","b","c","d", 1:9), 4, TRUE)),
                             collapse = ""),
                       ".R")
   init_file <- paste0(tempdir(),
                       "/init",
-                      paste(c(sample(c("a","b","c","d", 1:9), 4, TRUE)), 
+                      paste(c(sample(c("a","b","c","d", 1:9), 4, TRUE)),
                             collapse = ""),
                       ".R")
   out_file  = paste0(tempdir(),
                      "/out",
-                     paste(c(sample(c("a","b","c","d", 1:9), 4, TRUE)), 
+                     paste(c(sample(c("a","b","c","d", 1:9), 4, TRUE)),
                            collapse = ""),
                      ".csv")
 
-  
   if(score2 == "NULL") {
-    data_list <- df_to_list(df, 
+    data_list <- df_to_list(df,
                             IDp            = IDp,
                             IDs            = IDs,
                             times          = times,
@@ -388,10 +401,9 @@ sampling_gpu        <- function (df,
                             covb_nms       = covb_nms,
                             m_r            = m_r,
                             m_b            = m_b,
-                            is_pbo         = is_pbo,
-                            mod_name)
+                            is_pbo         = is_pbo)
   } else {
-    data_list <- df_to_list(df, 
+    data_list <- df_to_list(df,
                             IDp            = IDp,
                             IDs            = IDs,
                             times          = times,
@@ -406,8 +418,7 @@ sampling_gpu        <- function (df,
                             covb2_nms      = covb2_nms,
                             m_r2           = m_r2,
                             m_b2           = m_b2,
-                            is_pbo2        = is_pbo2,
-                            mod_name)
+                            is_pbo2        = is_pbo2)
   }
   maps      <- data_list$maps
   data_list <- data_list$data
@@ -422,14 +433,13 @@ sampling_gpu        <- function (df,
     stan_rdump(names(init_list), file = init_file)
   })
 
-  
   # here we have to check for OS and select the correct version into mod
   get_os <- function() {
-    if (.Platform$OS.type == "windows") { 
+    if (.Platform$OS.type == "windows") {
       "win"
     } else if (Sys.info()["sysname"] == "Darwin") {
-      "mac" 
-    } else if (.Platform$OS.type == "unix") { 
+      "mac"
+    } else if (.Platform$OS.type == "unix") {
       "unix"
     } else {
       stop("Unknown OS")
@@ -437,18 +447,24 @@ sampling_gpu        <- function (df,
   }
   my_os <- get_os()
   if (my_os == "win") {
-    mod <- system.file("bin",paste0(mod_name),"Win64",paste0(mod_name,".exe"), package = "GLMCPath")
-    mod <- paste0("\"", mod, "\"")      
+    if (score2 != "NULL") {
+      mod <- system.file("bin",paste0(mod_name),"Win64",paste0("glm_two_scores",".exe"), package = "GLMCPath")
+    } else {
+      mod <- system.file("bin",paste0(mod_name),"Win64",paste0("glm_one_score",".exe"), package = "GLMCPath")
+    }
   }
   if (my_os == "unix") {
-    mod <- system.file("bin",paste0(mod_name),"Linux",paste0(mod_name), package = "GLMCPath")
+    if (score2 != "NULL") {
+      mod <- system.file("bin",paste0(mod_name),"Linux",paste0("glm_two_scores"), package = "GLMCPath")
+    } else {
+      mod <- system.file("bin",paste0(mod_name),"Linux",paste0("glm_one_score"), package = "GLMCPath")
+    }
   }
   if (my_os == "mac") {
     stop("macOS not supported.")
-    # + link to page?
   }
 
-  
+
   # create string
   model_call <- paste0(mod,
                        " sample",
@@ -493,7 +509,7 @@ sampling_gpu        <- function (df,
     file.remove(data_file, init_file, out_file)
     stop(e)
   })
-  
+
 
   # read saved csv and return the values + delete temporary files
   tryCatch({
@@ -505,8 +521,74 @@ sampling_gpu        <- function (df,
     stop(paste(e, "No output from the model. Check model call."))
   })
 
-  return(list("stan_model" = stan_out, 
-              "data_used"  = data_list,
-              "maps"       = maps))
+
+  if(score2 == "NULL") {
+    ext <- rstan::extract(stan_out)
+    pred_df1 <- data.frame(IDp = data_list$IDp,
+                           IDs = data_list$IDs,
+                           time = data_list$time,
+                           scoreN = 1)
+    pred_df1 <- cbind(pred_df1, t(ext$score1_pred))
+    pred_df  <- pred_df1
+
+    mdf1 <- df[ , colnames(df) != "S2"]
+
+    # remove NAs
+    mdf1 <- mdf1[!is.na(mdf1$S1), ]
+
+    # rename columns
+    colnames(mdf1)[colnames(mdf1) == "S1"] <- "score"
+
+    # cbind score name
+    mdf1 <- cbind(mdf1, "scoreN" = 1)
+
+    # rbind
+    mdf <- mdf1
+
+    mdf_sorted     <- mdf[order(mdf$scoreN, mdf$IDp, mdf$IDs, mdf$time), ]
+    pred_df_sorted <- pred_df[order(mdf$scoreN, mdf$IDp, mdf$IDs, mdf$time), ]
+    out_data       <- cbind(mdf_sorted, pred_df_sorted[ ,-(1:4)])
+  } else {
+    ext <- rstan::extract(stan_out)
+    pred_df1 <- data.frame(IDp = data_list$IDp,
+                           IDs = data_list$IDs,
+                           time = data_list$time,
+                           scoreN = 1)
+    pred_df1 <- cbind(pred_df1, t(ext$score1_pred))
+
+    pred_df2 <- data.frame(IDp = data_list$IDp2,
+                           IDs = data_list$IDs2,
+                           time = data_list$time2,
+                           scoreN = 2)
+    pred_df2 <- cbind(pred_df2, t(ext$score2_pred))
+    pred_df  <- rbind(pred_df1, pred_df2)
+
+    mdf1 <- df[ , colnames(df) != "S2"]
+    mdf2 <- df[ , colnames(df) != "S1"]
+
+    # remove NAs
+    mdf1 <- mdf1[!is.na(mdf1$S1), ]
+    mdf2 <- mdf2[!is.na(mdf2$S2), ]
+
+    # rename columns
+    colnames(mdf1)[colnames(mdf1) == "S1"] <- "score"
+    colnames(mdf2)[colnames(mdf2) == "S2"] <- "score"
+
+    # cbind score name
+    mdf1 <- cbind(mdf1, "scoreN" = 1)
+    mdf2 <- cbind(mdf2, "scoreN" = 2)
+
+    # rbind
+    mdf <- rbind(mdf1, mdf2)
+
+    mdf_sorted     <- mdf[order(mdf$scoreN, mdf$IDp, mdf$IDs, mdf$time), ]
+    pred_df_sorted <- pred_df[order(mdf$scoreN, mdf$IDp, mdf$IDs, mdf$time), ]
+    out_data       <- cbind(mdf_sorted, pred_df_sorted[ ,-(1:4)])
+  }
+
+  return(list("stan_model"   = stan_out,
+              "data_used"    = data_list,
+              "maps"         = maps,
+              "pred_samples" = out_data))
 }
 
